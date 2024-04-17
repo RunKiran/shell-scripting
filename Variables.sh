57c9f219014006c5052b9128816ab3985683c022
RunKiran <kiranmadasu790@gmail.com>
2024-04-17 17:23:04 +0530

    some-message

--- Variables.sh
+++ Variables.sh
@@ -3,7 +3,7 @@
 p1=$1
 p2=$2
 
-echo  "p1 hello p2"
-echo  "p2 hi p2 How are you"
-echo  "p1 am good p2 what about you"
-echo  "p2 am also good p1"
+echo  "p1:: hello p2"
+echo  "p2:: hi p2 How are you"
+echo  "p1:: am good p2 what about you"
+echo  "p2:: am also good p1"
