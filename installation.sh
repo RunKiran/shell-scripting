333e2b7b3af138938809533cab2141634a3b962a
RunKiran <kiranmadasu790@gmail.com>
2024-04-18 14:00:01 +0530

    some-message

--- installation.sh
+++ installation.sh
@@ -8,6 +8,7 @@ if [$USERID -ne 0];then
    echo "to install root access needed"
    exit 1
 fi
+
 dnf install mysql -y
 
 if [$? -ne 0];then
@@ -25,8 +26,11 @@ fi
 dnf install git -y
 
 if [$? -ne 0];then
-   echo "installation of mysql failed"
+   echo "installation of git failed"
     else
-    echo "installation of mysql succes"
+    echo "installation of git succes"
+    else
+    echo "git already installed"
+
     exit 1
 fi
