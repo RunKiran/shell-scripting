02a86dba5219bf8c8130b3d736ecfd333ed0c6f9
RunKiran <kiranmadasu790@gmail.com>
2024-04-18 15:31:41 +0530

    some-message

--- functions.sh
+++ functions.sh
@@ -10,19 +10,21 @@ if [$USERID -ne 0];then
    echo "to install root access needed"
    exit 1
 fi
-
+Validation(){
 if [ $1 -ne 0 ];then
    echo "$2 failed"
+   exit 1
 else
     echo "$2 succes"
-    exit 0
+    exit 1
 fi
+}
 
 dnf install mysqll -y
-Validation $0  "installation of mysqll"
+Validation $0  "installation of mysql"
 
 
 
-dnf install git -y
+dnf install gitt -y
 Validation $0  "installation of git"
 
