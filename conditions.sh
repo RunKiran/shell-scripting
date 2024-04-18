fcbe35e3d2275e1a56373df4e575d4e37282731e
RunKiran <kiranmadasu790@gmail.com>
2024-04-18 13:32:12 +0530

    some-message

--- conditions.sh
+++ conditions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 NUMBER=5
-if(NUMBER>5){  
+if(NUMBER>5){
     print "Given number is graterthan 10" 
     }
     
-else {
+else{
     print  "Given number is lessthan 10"    
 }
 
-else {  
+else{  
     print "given number equal to 5"
 }
