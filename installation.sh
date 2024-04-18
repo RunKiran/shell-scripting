af110da7d71cd1b66b33077ee6132c8c40712e84
RunKiran <kiranmadasu790@gmail.com>
2024-04-18 13:56:43 +0530

    some-message

---
+++ installation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#installation of mysql and git
+
+USERID=(id -u)
+
+if [$USERID -ne 0];then
+   echo "to install root access needed"
+   exit 1
+fi
+dnf install mysql -y
+
+if [$? -ne 0];then
+   echo "installation of mysql failed"
+    else
+    echo "installation of mysql succes"
+    exit 1
+fi
+
+if [$USERID -ne 0];then
+   echo "to install root access needed"
+   exit 1
+fi
+
+dnf install git -y
+
+if [$? -ne 0];then
+   echo "installation of mysql failed"
+    else
+    echo "installation of mysql succes"
+    exit 1
+fi
