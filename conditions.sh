197dc524c66f1681f42283383e354b9064fab742
RunKiran <kiranmadasu790@gmail.com>
2024-04-18 13:42:01 +0530

    some-message

--- conditions.sh
+++ conditions.sh
@@ -3,6 +3,6 @@
 NUMBER=$1
 
 if [$NUMBER -gt 10 ];then
-    print "given number is graterthan 10"
+    echo "given number is graterthan 10"
  else
-    print "given number is lessthan 10"   
+    echo "given number is lessthan 10"
