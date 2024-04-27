94a2040ab3d53710770467ecca39f346fd945a4b
RunKiran <kiranmadasu790@gmail.com>
2024-04-27 19:38:16 +0530

    some-message

---
+++ delete-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE_DIR=
+if [ -d $SOURCE_DIR ]
+then
+    echo "source dir exsists"
+else
+    echo "Please make sure $SOURCE_DIR exsists"
+    exit 1
+fi
+files=$(find . -name "*.log" -mtime +14)
+echo "files to be delete $files"
+
+while IFS=read -r line
+do
+
+    echo "deleting files: $line"
+
+done<<<$files
